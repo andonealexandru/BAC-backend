14c2ebaf5b175b6c6503dabe87066dba601b0fad
Alexandru Andone <andonealexandru19@yahoo.com>
2020-06-22 22:46:23 +0300

    Commented for checking endpoint

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ app = Flask(__name__)
 app.config['MONGO_URI'] = "mongodb://localhost:27017/bac_test"
 mongo = PyMongo(app)
 
+
 @app.route("/upload", methods=['POST'])
 def handle_request():
-    img = request.get_json()
-    mongo.db.images.insert(dict(img))
-    img = dict(img)
-    with open("imageToSave.png", "wb") as fh:
-        fh.write(base64.b64decode(str(img["key"])))
+    # img = request.get_json()
+    # mongo.db.images.insert(dict(img))
+    # img = dict(img)
+    # with open("imageToSave.png", "wb") as fh:
+    #     fh.write(base64.b64decode(str(img["key"])))
     return "ok"
-    
+
+
 app.run(host="0.0.0.0", debug=False)
