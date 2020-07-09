436cac7c27ea666ee67362e43176f96aefb7451e
voltzer2 <43811028+h3xxit@users.noreply.github.com>
2020-07-09 19:16:09 +0300

    Second screen complet

--- app/src/main/java/com/example/myapplication/Second.java
+++ app/src/main/java/com/example/myapplication/Second.java
@@ -1,21 +1,34 @@
 package com.example.myapplication;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
-public class Second extends AppCompatActivity {
+public class Second extends AppCompatActivity{
 
+    Button btn_next, btn_back;
+    CodeEditText codeEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        btn_next = findViewById(R.id.btn_next);
+        btn_back = findViewById(R.id.btn_back);
+        codeEditText = (CodeEditText) findViewById(R.id.input_editText);
+
+
         Spinner dropdown = findViewById(R.id.spinner_select_compiler);
         String[] items = new String[]{"C", "C++", "Pascal"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
+
     }
+
 }
