e7eceaaf86d7a73e1b8531ba1c26cc04a971a7a5
voltzer2 <43811028+h3xxit@users.noreply.github.com>
2020-07-17 18:05:40 +0300

    Second screen

--- app/src/main/java/com/example/myapplication/Second.java
+++ app/src/main/java/com/example/myapplication/Second.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +14,7 @@ import android.widget.Spinner;
 public class Second extends AppCompatActivity{
 
     Button btn_next, btn_back;
+    Button btn_to_first_page, btn_to_third_page;
     CodeEditText codeEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,8 @@ public class Second extends AppCompatActivity{
 
         btn_next = findViewById(R.id.btn_next);
         btn_back = findViewById(R.id.btn_back);
+        btn_to_first_page = findViewById(R.id.btn_first_page);
+        btn_to_third_page = findViewById(R.id.btn_third_page);
         codeEditText = (CodeEditText) findViewById(R.id.input_editText);
 
 
@@ -28,6 +32,21 @@ public class Second extends AppCompatActivity{
         String[] items = new String[]{"C", "C++", "Pascal"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
+
+        btn_to_first_page.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Second.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn_to_third_page.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // Intent intent = new Intent(Second.this, MainActivity.class);
+               // startActivity(intent);
+            }
+        });
 
     }
 
