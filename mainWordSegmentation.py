8c99a4e13d6590e6b7f8e915e6a207ab54c8abbe
SoranaAurelia <sorana.aurelia@gmail.com>
2020-07-28 18:18:35 +0300

    added image constrast

--- mainWordSegmentation.py
+++ mainWordSegmentation.py
@@ -7,12 +7,49 @@ import Model
 
 images_for_model = []
 
+
 def send_words_to_nn():
 	list_word = getImages()
 	NN = Model.NeuralNetwork(create=False)
 	for word in list_word:
 		interpreted_text = NN.return_text(word)
 		print(interpreted_text)
+
+
+def increase_contrast_and_apply_treshold():
+	img = cv2.imread('data/test_cod.jpeg', 1)
+	img = cv2.GaussianBlur(img, (5, 5), 0)
+
+	# -----Converting image to LAB Color model-----------------------------------
+	lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+		# -----Splitting the LAB image to different channels-------------------------
+	l, a, b = cv2.split(lab)
+
+	# -----Applying CLAHE to L-channel-------------------------------------------
+	clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+	cl = clahe.apply(l)
+
+	# -----Merge the CLAHE enhanced L-channel with the a and b channel-----------
+	limg = cv2.merge((cl, a, b))
+
+	# -----Converting image from LAB Color model to RGB model--------------------
+	final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+
+	# read
+	kernel = np.ones((3, 3))
+	img = cv2.erode(final, kernel)
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	# cv2.imwrite('erode.png', img)
+
+	# ret, thresh1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+	# # cv2.imwrite('treshhold.png', thresh1)
+	#
+	# th2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 61, 8)
+	# cv2.imwrite('treshhold2.png', th2)
+
+	th3 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 61, 14)
+	# cv2.imwrite('treshold3.png', th3)
+	return th3
 
 
 def getImages():
@@ -24,8 +61,8 @@ def getImages():
 		print('Segmenting words of sample %s'%f)
 		
 		# read image, prepare it by resizing it to fixed height and converting it to grayscale
-		img = prepareImg(cv2.imread('data/%s'%f), 270)
-		
+		img = increase_contrast_and_apply_treshold()
+		img = prepareImg(img, 270)
 		# execute segmentation with given parameters
 		# -kernelSize: size of filter kernel (odd integer)
 		# -sigma: standard deviation of Gaussian function used for filter kernel
@@ -41,30 +78,30 @@ def getImages():
 		print('Segmented into %d words'%len(res))
 		for (j, w) in enumerate(res):
 			(wordBox, wordImg) = w
-			(x, y, w, h) = wordBox
+			# (x, y, w, h) = wordBox
 			cv2.imwrite('out/%s/%d.png'%(f, j), wordImg) # save word
-			cv2.rectangle(img,(x,y),(x+w,y+h),0,1) # draw bounding box in summary image
+			# cv2.rectangle(img,(x,y),(x+w,y+h),0,1) # draw bounding box in summary image
 
 			img_resized = DataLoader.extract_img(wordImg)
 
-			pxmin = np.min(img_resized)
-			pxmax = np.max(img_resized)
-			imgContrast = (img_resized - pxmin) / (pxmax - pxmin) * 255
-
-			# increase line width
-			kernel = np.ones((3, 3), np.uint8)
-			imgMorph = cv2.erode(imgContrast, kernel, iterations=1)
+			# pxmin = np.min(img_resized)
+			# pxmax = np.max(img_resized)
+			# imgContrast = (img_resized - pxmin) / (pxmax - pxmin) * 255
+			#
+			# # increase line width
+			# kernel = np.ones((3, 3), np.uint8)
+			# imgMorph = cv2.erode(imgContrast, kernel, iterations=1)
 
 			# write
-			cv2.imwrite('out/%s/%d.png'%(f, j), imgMorph)
+			cv2.imwrite('out/%s/%d.png'%(f, j), img_resized)
 
-			images_for_model.append(imgMorph)
+			images_for_model.append(img_resized)
 
 		# output summary image with bounding boxes around words
 		cv2.imwrite('out/%s/summary.png'%f, img)
 		print(len(images_for_model))
 		return images_for_model
-
-if __name__ == '__main__':
-	getImages()
-	send_words_to_nn()
+#
+# if __name__ == '__main__':
+# 	getImages()
+# 	send_words_to_nn()
