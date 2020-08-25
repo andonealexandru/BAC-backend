3896e57ab46d524f9bb1b3d7e2cf9f2fae653356
SoranaAurelia <sorana.aurelia@gmail.com>
2020-08-25 15:10:25 +0300

    addaugare date train mate,

--- DataLoader.py
+++ DataLoader.py
@@ -3,10 +3,84 @@ import os
 import numpy as np
 from Alphabet import text_to_label
 
-path_to_file = '/home/andone/Documents/Programming/python/database for train/ascii/words.txt'  # path catre words.txt
-path_to_folder = '/home/andone/Documents/Programming/python/database for train/words/'  # path catre folderul cu cuvinte
-num_images = 115000  # pe cate imagini sa se ia din words (pe cate sa antrenam)
+path_to_file = 'D:/Handwriting DB/ascii/words.txt'  # path catre words.txt
+path_to_folder = 'D:/Handwriting DB/words/'  # path catre folderul cu cuvinte
+num_images = 1000  # pe cate imagini sa se ia din words (pe cate sa antrenam)
 
+path_to_file_math = 'D:/dpit2020/jungomi-datasets-crohme-png-1/groundtruth_train.tsv'
+path_to_folder_math = 'D:/dpit2020/jungomi-datasets-crohme-png-1/train/'
+
+
+def get_x_y_math():
+    print('hi')
+    f = open(path_to_file_math)
+    count = 0
+    x = []  # imaginile
+    y = []  # in y sunt labelurile
+    x_new = []
+    im_path = []
+    for i, line in enumerate(f):
+        try:
+            # if i>27:
+            #     continue
+            lineSplit = line.strip().split('\t')
+            img_path = path_to_folder_math+lineSplit[0]+'.png'
+            img = cv2.imread(img_path)
+            # convert image:
+            img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            cv2.imwrite('test_crop_init.png', img)
+
+            img = np.asarray(img)
+            # trim image to only contain the text:
+            non_empty_columns = np.where(img.min(axis=0) <255)[0]
+            non_empty_rows = np.where(img.min(axis=1) <255)[0]
+            cropBox = (min(non_empty_rows), max(non_empty_rows), min(non_empty_columns), max(non_empty_columns))
+
+            image_data_new = img[cropBox[0]:cropBox[1] + 1, cropBox[2]:cropBox[3] + 1]
+
+            folder_img = lineSplit[0].strip().split('/')
+            if folder_img[0] == 'MathBrush':
+                img = img
+            else:
+                ret, img = cv2.threshold(image_data_new, 120, 255, cv2.THRESH_BINARY)
+            cv2.imwrite('test_crop.png', img)
+            img2 = extract_img(img)
+            if folder_img[0] == 'MathBrush':
+                kernel = np.ones((2, 2))
+                img = cv2.erode(image_data_new, kernel, iterations=1)
+
+                img = extract_img(img)
+                kernel = np.ones((2, 2))
+                img = cv2.erode(img, kernel, iterations=1)
+                ret, img = cv2.threshold(img, 250, 255, cv2.THRESH_BINARY)
+            else:
+                img = cv2.adaptiveThreshold(img2.astype('uint8'), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 5, 2)
+                kernel = np.ones((2, 2))
+                img = cv2.erode(img, kernel, iterations=1)
+            # img = cv2.erode(th3, (2, 2), iterations=1)
+            # print(folder_img[0] +' '+ folder_img[1])
+            cv2.imwrite('umm.png', img)
+            if not os.path.exists('data_out_mate/%s' % folder_img[0]):
+                os.mkdir('data_out_mate/%s' % folder_img[0])
+            cv2.imwrite('data_out_mate/%s/%s.png'%(folder_img[0], folder_img[1]), img.astype('uint8'))
+
+            x.append(img)
+            text = lineSplit[1]
+            if folder_img[0] != 'Hamex':
+                text = text.replace(' ', '')
+            if folder_img[0] == 'MathBrush':
+                text = text.replace('{', '')
+                text = text.replace('}', '')
+                text = text.replace(' ', '')
+            y.append(text)
+            # print(text)
+
+
+            if i%100 == 0:
+                print(i)
+        except Exception as e:
+            print(e)
+    return x, y
 
 def get_x_y(evaluate, nr_img):
     f = open(path_to_file)
@@ -50,7 +124,7 @@ def get_x_y(evaluate, nr_img):
 
 def create_input_label_length_and_labels(y, evaluate, nr_img):
     y2 = []
-    input_lengths = np.ones((num_images, 1)) * 32
+    input_lengths = np.ones((num_images, 1)) * 64
     label_lengths = np.zeros((num_images, 1))
 
     maxim = num_images
@@ -63,18 +137,26 @@ def create_input_label_length_and_labels(y, evaluate, nr_img):
             print(val)
         y2.append(val)
         label_lengths[i] = len(y[i])
-        input_lengths[i] = 32
+        input_lengths[i] = 64
     y2 = np.asarray(y2)
     return input_lengths, label_lengths, y2
 
 
 def get_data(evaluate, nr_img):
     x, x_new, y = get_x_y(evaluate, nr_img)
+    x_mate, y_mate = get_x_y_math()
     input_lengths, label_lengths, y2 = create_input_label_length_and_labels(y, evaluate, nr_img)
+    input_lengths2, label_lengths2, y22 = create_input_label_length_and_labels(y_mate, evaluate, nr_img)
 
     maxim = num_images
     if evaluate:
         maxim = nr_img
+    x_mate = np.asarray(x_mate)
+    x_mate = x_mate[:, :, :, np.newaxis]
+    x_mate = np.transpose(x_mate, (0, 2, 1, 3))
+    x_mate = np.array(x_mate)/255
+
+    y_mate = np.asarray(y22)
 
     x = np.asarray(x_new[:maxim])  # pastrez doar de cate am nevoie
     x = x[:, :, :, np.newaxis]  # Bx32x128 -> Bx32x128x1
@@ -90,7 +172,7 @@ def get_data(evaluate, nr_img):
     input_lengths = input_lengths[:maxim]
     label_lengths = label_lengths[:maxim]
 
-    return x2, y2, input_lengths, label_lengths
+    return x2, y2, input_lengths, label_lengths, x_mate, y_mate, input_lengths2, label_lengths2
 
 
 def extract_img(img):
--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 from Model import NeuralNetwork
 from mainWordSegmentation import getImages, send_words_to_nn
-from DataLoader import get_data
+from DataLoader import get_data, get_x_y_math, get_x_y
 from DataLoader import prepare_image
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
 
-images, labels, input_lengths, label_Lengths = get_data(evaluate=False, nr_img=1000)
+x, y, input_lengths1, label_Lengths1, x_mate, y_mate, input_lengths2, label_lengths2 = get_data(evaluate=False, nr_img=1000)
+x = np.array(x)
+x_mate = np.array(x_mate)
+images = np.concatenate((x, x_mate))
+labels = np.concatenate((y, y_mate))
+input_lengths = np.concatenate((input_lengths1, input_lengths2))
+label_Lengths = np.concatenate((label_Lengths1, label_lengths2))
+
 images = np.asarray(images)
 labels = np.asarray(labels)
 input_lengths = np.asarray(input_lengths)
 label_Lengths = np.asarray(label_Lengths)
 
-# print(images.shape)
-# print(labels.shape)
-# print(input_lengths.shape)
-# print(label_Lengths.shape)
-
-nn = NeuralNetwork(create=True, batch_norm=True, dropout=0.5)
-# nn.evaluate_model(images, labels, input_lengths, label_Lengths)
-
-nn.train(train_images=images, train_labels=labels,
-         validation_split=0.15,
-         input_length=input_lengths, label_length=label_Lengths,
-         batch_size=32, epochs=5)
-
-print(send_words_to_nn())
+num = len(images)
+places = np.arange(num)
+images = images[places]
