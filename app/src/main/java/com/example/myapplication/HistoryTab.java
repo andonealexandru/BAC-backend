2d6116cbd9b33de4adb32aba3efe6b67982cd23c
CalinNavadaru <66365479+CalinNavadaru@users.noreply.github.com>
2020-09-02 12:16:27 +0300

    CEVA

--- app/src/main/java/com/example/myapplication/HistoryTab.java
+++ app/src/main/java/com/example/myapplication/HistoryTab.java
@@ -117,7 +117,7 @@ public class HistoryTab extends AppCompatActivity {
     void connectServer(String data){
 
 
-        String postUrl= "http://bac-advanced-compiler.herokuapp.com//retrieve_history";
+        String postUrl= "http://bac-advanced-compiler.herokuapp.com/retrieve_history";
 
 
         JSONObject imageJSON = new JSONObject();
