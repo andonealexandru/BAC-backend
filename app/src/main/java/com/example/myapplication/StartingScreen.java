e71eb3b4631cc2a359ae603fdb837a7c34df7ae3
SoranaAurelia <sorana.aurelia@gmail.com>
2020-09-19 18:44:58 +0300

    progress-bar

--- app/src/main/java/com/example/myapplication/StartingScreen.java
+++ app/src/main/java/com/example/myapplication/StartingScreen.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.bumptech.glide.Glide;
 
@@ -14,17 +16,14 @@ public class StartingScreen extends Activity {
     /** Duration of wait **/
     private final int SPLASH_DISPLAY_LENGTH = 5000;
     ImageView gifLoading;
-
+    ProgressBar pb;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.starting_screen);
-        gifLoading = findViewById(R.id.gif_loading);
-
-        Glide.with(this)
-                .load(R.drawable.test_loading_gif)
-                .into(gifLoading);
+        pb = findViewById(R.id.progressBar2);
+        pb.setVisibility(View.VISIBLE);
 
         /* New Handler to start the Menu-Activity
          * and close this Splash-Screen after some seconds.*/
