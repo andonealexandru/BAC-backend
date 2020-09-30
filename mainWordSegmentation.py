6133766a70253a621ad83132115910c9f6d3a689
Alexandru Andone <andonealexandru19@yahoo.com>
2020-09-30 20:43:22 +0300

    send words to nn

--- mainWordSegmentation.py
+++ mainWordSegmentation.py
@@ -12,9 +12,9 @@ considered_indent = 60
 
 def send_words_to_nn(img):
     list_word, num_with = getImages(img)
-    NN = NeuralNetwork(create=False)
-    model = retrieve_model()
-    model2 = keras.Model(model.get_layer('input').input, model.layers[14].output)
+    NN = NeuralNetwork(create=False, batch_norm=True, dropout=0.5)
+    model = retrieve_model_with_create_arhitecture()
+    model2 = keras.Model(model.get_layer('input').input, model.get_layer("time_distributed_last").output)
     text = ''
     for i in range(0, num_with):
         print(i)
